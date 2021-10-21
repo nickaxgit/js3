5d1188e4b493a25c1eac89f1711edf2bfd11f1c5
nickaxgit <nickax@gmail.com>
2021-10-21 09:39:46 +0100

    Finished task

---
+++ script.js
@@ -0,0 +1,29 @@
+// Determine if a number is odd or even
+let j=prompt("Enter a number","2")
+if(j % 2 ){
+    alert("odd")    
+}
+else {
+    alert("even")
+}
+
+//Is one number divisible by another
+let a=prompt("first")
+let b=prompt("second")
+
+if(a % b){ //Checks the remainder of a/b
+    alert("Indivisble")
+}
+else{    
+    alert("Divisible")
+}
+
+
+//344 students to the theatre, if each bus can take 42 people - how many busses
+alert(Math.ceil(344/42))
+
+
+
+
+
+
